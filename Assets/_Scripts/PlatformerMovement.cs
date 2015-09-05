02fb843429fa73900fbe278fc87402b220b01666
Ramses di Perna <15826@ma-web.nl>
2015-09-05 17:32:16 +0200

    Backup before big change in platform movement

--- Assets/_Scripts/PlatformerMovement.cs
+++ Assets/_Scripts/PlatformerMovement.cs
@@ -3,15 +3,31 @@ using System.Collections;
 
 public class PlatformerMovement : MonoBehaviour {
 
+	// public constants
 	public static int DIR_RIGHT = 1;
 	public static int DIR_LEFT = -1;
 
+	// public delegates and events
+	public delegate void GameObjectDelegate (GameObject obj);
+
+	public event GameObjectDelegate LandedOnGround;
+	public event GameObjectDelegate ReleasedFromGround;
+
+	public event GameObjectDelegate StartedSideTouch;
+	public event GameObjectDelegate EndedSideTouch;
+
+	// Current state.
 	private bool _inWallSlide = false;
 	private bool _onGround = false;
 
+	// Collider variables
 	private BoxCollider2D colliderBox;
 	private Vector2 centerCollider;
 	private Vector2 sizeCollider;
+
+	// Remember Data
+	private GameObject _preGround;
+
 
 	void Start(){
 		colliderBox = GetComponent<BoxCollider2D> ();
@@ -25,13 +41,20 @@ public class PlatformerMovement : MonoBehaviour {
 
 	void Update(){
 		RaycastHit2D hit;
-			//check if landed on an object
+		//check if landed on an object
 		hit = Physics2D.Raycast(centerCollider,Vector2.down,sizeCollider.y / 2);
 
-		if(hit.collider != null){
+		if(hit.collider != null && !_onGround){
 			_onGround = true;
-		}else{
+			if(LandedOnGround != null){
+				LandedOnGround(hit.collider.gameObject);
+			}
+			_preGround = hit.collider.gameObject;
+		}else if(_onGround){
 			_onGround = false;
+			if(ReleasedFromGround != null){
+				ReleasedFromGround(_preGround);
+			}
 		}
 		if (!_onGround) {
 			//check wallslide.
@@ -47,5 +70,12 @@ public class PlatformerMovement : MonoBehaviour {
 				}
 			}
 		}
+	}
+
+	public bool onGround{
+		get{return _onGround;}
+	}
+	public bool sideTouching{
+		get{return _inWallSlide;}
 	}
 }
