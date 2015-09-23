792e321ade698ff956f28ee073c63f4a25eb5389
menno <run_jongejan@hotmail.com>
2015-09-23 11:43:25 +0200

    pause menu added + sound volume handler

---
+++ Assets/SoundVolumeHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SoundVolumeHandler : MonoBehaviour {
+	public Slider mainAudioSlider;
+	public Slider fxAudioSlider;
+	public void OnMainAudioSliderChange()
+	{
+		AudioPlayer.backgroundVolume = mainAudioSlider.value;
+	}
+	public void OnFXAudioSliderChange()
+	{
+		AudioPlayer.fxVolume = fxAudioSlider.value;
+	}
+}
